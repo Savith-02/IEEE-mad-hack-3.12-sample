6c3a79c1b62b031cee84e9f55999389b093b35a4
Savith Panamgama <geethikasavith@gmail.com>
2024-09-14 03:57:52 +0530

    refactor: Update CSS styles for responsiveness

--- src/components/Collapsible/Collapsible.js
+++ src/components/Collapsible/Collapsible.js
@@ -12,6 +12,12 @@ const Collapsible = ({ question, answer }) => {
     <div className="collapsible">
       <button className="collapsible-header" onClick={toggleCollapsible}>
         {question}
+        {/* <i className={`fas fa-chevron-down ${isOpen ? 'rotate' : ''}`}></i> */}
+        {isOpen ? (
+          <i className="fas fa-chevron-up"></i>
+        ) : (
+          <i className="fas fa-chevron-down"></i>
+        )}
       </button>
       {isOpen && <div className="collapsible-content">{answer}</div>}
     </div>
--- src/views/FAQ/data.js
+++ src/views/FAQ/data.js
@@ -1,14 +1,22 @@
 export const faqs = [
   {
-    Q: 'What is MadHACK?',
-    Ans: 'MadHACK is a hackathon event focused on mobile app development.',
+    Q: 'How do I register for MadHACK?',
+    Ans: 'To register for MadHACK, you can visit our website and fill out the registration form. Make sure to provide accurate information and read the terms and conditions before submitting your registration.',
   },
   {
-    Q: 'Who can participate?',
-    Ans: 'Anyone with an interest in mobile app development can participate.',
+    Q: 'Can I participate as an individual or do I need a team?',
+    Ans: 'You can participate either as an individual or as a team. If you prefer to work alone, you can register as an individual participant. If you have a team, you can register together and collaborate on your project.',
   },
   {
-    Q: 'What are the prizes?',
-    Ans: 'Prizes include cash awards for the top three teams.',
+    Q: 'What are the judging criteria?',
+    Ans: 'The judging criteria for MadHACK include creativity, functionality, user experience, and technical implementation. Judges will evaluate each project based on these criteria to determine the winners.',
+  },
+  {
+    Q: 'Are there any restrictions on the technologies or platforms we can use?',
+    Ans: 'There are no specific restrictions on the technologies or platforms you can use for your mobile app. You are free to choose any programming language, framework, or platform that you are comfortable with.',
+  },
+  {
+    Q: 'Can I submit a project that I have previously worked on?',
+    Ans: 'No, the projects submitted for MadHACK must be original and created specifically for the hackathon. Any projects that have been previously developed or submitted to other competitions will not be eligible for participation.',
   },
 ];
