48aa8f894cee8df62c7c237cc34d509c1a001e84
Savith Panamgama <geethikasavith@gmail.com>
2024-09-14 04:34:07 +0530

    refactor: Lazy load components and add suspense fallback in App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
+import React, { Suspense, lazy } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Notfound from './components/NotFound/NotFound';
-import UnderDev from './components/underDev/UnderDev';
 import Main from './views/Main/Main';
+
+// Lazy load components
+const Notfound = lazy(() => import('./components/NotFound/NotFound'));
+const UnderDev = lazy(() => import('./components/underDev/UnderDev'));
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/student-requests" element={<UnderDev />} />
-          <Route path="/create-assistant" element={<UnderDev />} />
-          <Route path="/prizes" element={<UnderDev />} />
-          <Route path="/faq" element={<UnderDev />} />
-          <Route path="/contact-us" element={<UnderDev />} />
-          <Route path="/task" element={<UnderDev />} />
-          <Route path="*" element={<Notfound />} />
-        </Routes>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/student-requests" element={<UnderDev />} />
+            <Route path="/create-assistant" element={<UnderDev />} />
+            <Route path="/prizes" element={<UnderDev />} />
+            <Route path="/faq" element={<UnderDev />} />
+            <Route path="/contact-us" element={<UnderDev />} />
+            <Route path="/task" element={<UnderDev />} />
+            <Route path="*" element={<Notfound />} />
+          </Routes>
+        </Suspense>
         <Footer />
       </div>
     </Router>
